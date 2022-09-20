1f95c9daaf89660afb33d1873a9a9c2b18ddda6a
Liem Dam-Quang <liem.dam-quang@mail.mcgill.ca>
2022-09-20 00:29:52 -0400

    first commit

---
+++ goddard/rocket.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cantera as ct 
+
+def calculate_c_star(gamma, temperature, molecular_weight):
+    '''Calculate the C* value for a given combustion temperature and set of gas properties. C* is also known as the characteristic velocity,
+    and is a measure of the energy present in the fluid as a result of combustion. It is used as a metric for engine performance independent
+    of nozzle expansion.
+    '''
+    return (
+        np.sqrt(ct.gas_constant * temperature / (molecular_weight * gamma)) *
+        np.power(2 / (gamma + 1), -(gamma + 1) / (2*(gamma - 1)))
+        )
+
