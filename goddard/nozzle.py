Fixed bug leading to incorrect results for combustion of condensed phases. This is a known issue with Cantera: see https://github.com/Cantera/cantera/issues/270.
Additionally, fixed incorrect convergence algorithm for nozzle exit conditions for frozen flow. The Gordon and McBride paper was somewhat ambiguous about it, and it took a few tries
before landing on the correct one.

goddard/nozzle.py
@@ -11,7 +11,7 @@ from thermo import get_thermo_derivatives, get_thermo_properties
 from utils import to_si, copy_solution
 from pint import Quantity
 
-def calculate_throat_conditions(gas: ct.Solution, P_chamber: Quantity, gas_chamber: ct.Solution, gamma_chamber):
+def get_throat_conditions(gas: ct.Solution, P_chamber: Quantity, gas_chamber: ct.Solution, gamma_chamber):
     '''
     Assumptions: 
     - Flow is isentropic
@@ -27,6 +27,7 @@ def calculate_throat_conditions(gas: ct.Solution, P_chamber: Quantity, gas_chamb
     M = 1.0 #throat mach is 1.0 by definition
     num_iter = 0
     residual = 1
+    # print(f"P throat: {P_throat}")
     while residual > tolerance_throat:
         num_iter += 1
         if num_iter == max_iter_throat:
@@ -34,20 +35,22 @@ def calculate_throat_conditions(gas: ct.Solution, P_chamber: Quantity, gas_chamb
             break
 
         P_throat = P_throat * (1 + gamma_s * M**2)/(1 + gamma_s)
+        # print(f"P_throat: {to_si(P_throat)}")
         gas.SPX = gas_chamber.s, to_si(P_throat), gas_chamber.X
         gas.equilibrate('SP')
         derivs = get_thermo_derivatives(gas)
         dlogV_dlogT_P, dlogV_dlogP_T, cp, gamma_s = get_thermo_properties(gas)
 
-        velocity = _calculate_velocity(gas, gas_chamber.h)
-        sonic_velocity = _calculate_sonic_velocity(gas, gamma_s)
+        velocity = _get_velocity(gas, gas_chamber.h)
+        sonic_velocity = _get_sonic_velocity(gas, gamma_s)
+        # print(f"Velocity: {velocity}, sonic velocity: {sonic_velocity}")
         M = velocity/sonic_velocity
 
         residual = np.abs(1.0 - 1/M**2)
     
     return gas
 
-def calculate_exit_conditions(gas_throat: ct.Mixture, area_ratio, P_chamber, gas_chamber: ct.Solution, gamma_s, velocity):
+def get_exit_conditions(gas_throat: ct.Mixture, area_ratio, P_chamber, gas_chamber: ct.Solution, gamma_s, velocity):
     '''
     Calculate exit conditions, based on expansion area ratio.
     '''
@@ -77,8 +80,8 @@ def calculate_exit_conditions(gas_throat: ct.Mixture, area_ratio, P_chamber, gas
 
         derivs = get_thermo_derivatives(gas_exit)
         dlogV_dlogT_P, dlogV_dlogP_T, cp, gamma_s = get_thermo_properties(gas_exit)
-        velocity = _calculate_velocity(gas_exit, gas_chamber.h)
-        sonic_velocity = _calculate_sonic_velocity(gas_exit, gamma_s)
+        velocity = _get_velocity(gas_exit, gas_chamber.h)
+        sonic_velocity = _get_sonic_velocity(gas_exit, gamma_s)
 
         Ae_At = gas_exit.T / (gas_exit.P * velocity * gas_exit.mean_molecular_weight) / A_mdot_thr
         dlogp_dlogA = gamma_s * velocity** 2 / (velocity**2 - sonic_velocity**2)
@@ -93,10 +96,10 @@ def calculate_exit_conditions(gas_throat: ct.Mixture, area_ratio, P_chamber, gas
     
     return gas_exit
 
-def calculate_exit_conditions_pressure_ratio(gas_throat: ct.Mixture, pressure_ratio):
+def get_exit_conditions_pressure_ratio(gas_throat: ct.Mixture, pressure_ratio):
     pass
 
-def calculate_throat_conditions_frozen(gas: ct.Mixture, P_chamber: Quantity, gas_chamber: ct.Mixture, gamma_chamber):
+def get_throat_conditions_frozen(gas: ct.Mixture, P_chamber: Quantity, gas_chamber: ct.Mixture, gamma_chamber):
     """
     Throat conditions for frozen flow. See Gordon & McBride, 1994, Section 6.5.2.
     """
@@ -123,8 +126,8 @@ def calculate_throat_conditions_frozen(gas: ct.Mixture, P_chamber: Quantity, gas
 
         # gas.equilibrate('SP')
         # derivs = get_thermo_derivatives(gas)
-        velocity = _calculate_velocity(gas, gas_chamber.h)
-        sonic_velocity = _calculate_sonic_velocity(gas, gamma_s)
+        velocity = _get_velocity(gas, gas_chamber.h)
+        sonic_velocity = _get_sonic_velocity(gas, gamma_s)
         M = velocity/sonic_velocity
 
         residual = np.abs(1.0 - 1/M**2)
@@ -133,19 +136,23 @@ def calculate_throat_conditions_frozen(gas: ct.Mixture, P_chamber: Quantity, gas
 
 
 
-def calculate_exit_conditions_frozen(gas_throat: ct.Mixture, area_ratio, p_chamber, gas_chamber: ct.Solution, gamma_s, velocity):
+def get_exit_conditions_frozen(gas_throat: ct.Mixture, area_ratio, p_chamber, gas_chamber: ct.Solution, gamma_s, velocity):
     """
     See Gordon & McBride, 1994, Section 6.5.
     """
     A_mdot_thr = gas_throat.T / (gas_throat.P * velocity * gas_throat.mean_molecular_weight) #remains constant
     # we iterate to obtain the nozzle exit temperature 
-    T_e = gas_chamber.T #initial guess
+    T_e = gas_throat.T #initial guess
     gas_exit = copy_solution(gas_throat)
 
     gamma_s = gas_exit.cp/gas_exit.cv
     
     pressure_ratio = np.exp(gamma_s + 1.4 * np.log(area_ratio)) #initial guess 
+    # print(f"Area ratio: {area_ratio}")
+    # print(f"P_chamber: {p_chamber}")
+    # print(f"Pressure ratio: {pressure_ratio}")
     p_exit = to_si(p_chamber)/pressure_ratio
+    # print(f"P_exit: {p_exit}")
     gas_exit.TPX = T_e, p_exit, gas_exit.X
 
     gamma_s = gas_exit.cp/gas_exit.cv
@@ -153,36 +160,101 @@ def calculate_exit_conditions_frozen(gas_throat: ct.Mixture, area_ratio, p_chamb
     dlnT = (gas_chamber.s - gas_exit.s)/gas_exit.cp
 
     dlogT_tolerance = 0.5e-4
-    maxiter = 50
+    pressure_tolerance = 0.4e-4
+    maxiter = 10
+    maxiter_temp = 8
     n = 0
-    while np.abs(dlnT) >= dlogT_tolerance:
+
+    residual = 1
+    while np.abs(residual) > pressure_tolerance:
         n += 1
-        if n >= maxiter:
-            #TODO: error message
+        if n == maxiter:
+            #TODO: error message if nozzle fails to converge
             break
+        # print(f"Iteration: {n}")
+        m = 0
+        dlnT = 1000
+        while np.abs(dlnT) >= dlogT_tolerance:
+            m += 1
+            if m >= maxiter_temp:
+                break
+            gas_exit.TPX = T_e, p_exit, gas_exit.X
+            dlnT = (gas_chamber.s - gas_exit.s)/gas_exit.cp
+            T_e = np.exp(np.log(T_e) + dlnT)
+            # print(f"T_exit: {T_e}")
 
-        gas_exit.TPX = T_e, p_exit, gas_exit.X
-        dlnT = (gas_chamber.s - gas_exit.s)/gas_exit.cp
-        gamma_s = gas_exit.cp/gas_exit.cv
+        gamma_s = gas_exit.cp/gas_exit.cv #gamma_s = gamma for frozen flow
+        velocity = _get_velocity(gas_exit, gas_chamber.h)
+        sonic_velocity = _get_sonic_velocity(gas_exit, gamma_s)
 
-        velocity = _calculate_velocity(gas_exit, gas_chamber.h)
-        sonic_velocity = _calculate_sonic_velocity(gas_exit, gamma_s)
         Ae_At = gas_exit.T / (gas_exit.P * velocity * gas_exit.mean_molecular_weight) / A_mdot_thr
         dlogp_dlogA = gamma_s * velocity** 2 / (velocity**2 - sonic_velocity**2)
         residual = dlogp_dlogA * (np.log(area_ratio) - np.log(Ae_At))
         log_pinf_pe = np.log(pressure_ratio) + residual
 
+        
+        # print(f"Velocity: {velocity}")
+        # print(f"Gamma: {gamma_s}")
+        # print(f"Sonic velocity: {sonic_velocity}")
+        # print(f"dlogp_dlogA: {dlogp_dlogA}")
+
+        # print(f"Ae_at: {Ae_At}")
+        # print(f"Pressure residual: {residual}")
+
         pressure_ratio = np.exp(log_pinf_pe)
         p_exit = to_si(p_chamber) /  pressure_ratio
+        # print(f"Pressure ratio: {pressure_ratio}")
         
-        lnT_e = np.log(T_e) + dlnT
-        T_e = np.exp(lnT_e)
+        
+        # gas_exit()
+        # print("////////////////////////////")
+    
+
+    # while np.abs(dlnT) >= dlogT_tolerance:
+    #     n += 1
+    #     if n >= maxiter:
+    #         #TODO: error message
+    #         break
+    #     gas_exit.TPX = T_e, p_exit, gas_exit.X
+    #     print(f"Iteration: {n}")
+    #     gas_exit()
+    #     dlnT = (gas_chamber.entropy_mass - gas_exit.entropy_mass)/gas_exit.cp_mass
+    #     print(f"Temperature residual: {dlnT}")
+    #     gamma_s = gas_exit.cp/gas_exit.cv
+
+    #     velocity = _get_velocity(gas_exit, gas_chamber.enthalpy_mass)
+    #     sonic_velocity = _get_sonic_velocity(gas_exit, gamma_s)
+
+    #     Ae_At = gas_exit.T / (gas_exit.P * velocity * gas_exit.mean_molecular_weight) / A_mdot_thr
+    #     dlogp_dlogA = gamma_s * velocity**2 / (velocity**2 - sonic_velocity**2)
+    #     residual = dlogp_dlogA * (np.log(area_ratio) - np.log(Ae_At))
+
+    #     print(f"Velocity: {velocity}")
+    #     print(f"Gamma: {gamma_s}")
+    #     print(f"Sonic velocity: {sonic_velocity}")
+    #     print(f"dlogp_dlogA: {dlogp_dlogA}")
+
+    #     print(f"Ae_at: {Ae_At}")
+    #     print(f"Pressure residual: {residual}")
+    #     log_pinf_pe = np.log(pressure_ratio) + residual
+
+        
+
+
+    #     pressure_ratio = np.exp(log_pinf_pe)
+    #     print(f"Pressure ratio: {pressure_ratio}")
+    #     p_exit = to_si(p_chamber) /  pressure_ratio
+        
+    #     lnT_e = np.log(T_e) + dlnT
+    #     T_e = np.exp(lnT_e)
+    #     print(f"T_exit: {T_e}")
+    #     print("////////////////////////////")
 
     print(f"number of iterations: {n}")
 
     return gas_exit
     
-def calculate_exit_conditions_pressure_ratio_frozen(gas_throat: ct.Mixture, pressure_ratio, p_chamber, gas_chamber: ct.Solution, velocity):
+def get_exit_conditions_pressure_ratio_frozen(gas_throat: ct.Mixture, pressure_ratio, p_chamber, gas_chamber: ct.Solution, velocity):
     # we iterate to obtain the nozzle exit temperature 
     
     T_e = gas_chamber.T #initial guess
@@ -212,18 +284,18 @@ def calculate_exit_conditions_pressure_ratio_frozen(gas_throat: ct.Mixture, pres
 def _estimate_pressure_ratio():
     pass
 
-def _calculate_velocity(gas: ct.Mixture, stagnation_enthalpy):
+def _get_velocity(gas: ct.Mixture, stagnation_enthalpy):
     '''
     Velocity in isentropic supersonic flow can be found from the difference in enthalpy
     between the gas in motion and the stagnation enthalpy. 
     '''
     return np.sqrt(2*(stagnation_enthalpy - gas.enthalpy_mass))
 
-def _calculate_sonic_velocity(gas: ct.Solution, gamma):
+def _get_sonic_velocity(gas: ct.Solution, gamma):
     return np.sqrt(ct.gas_constant * gas.T * gamma / gas.mean_molecular_weight)
 
 
-def calculate_cstar(gamma, temperature, molecular_weight):
+def get_cstar(gamma, temperature, molecular_weight):
     '''
     Calculate the C* value for a given combustion temperature and set of gas properties. C* is also known as the characteristic velocity,
     and is a measure of the energy present in the fluid as a result of combustion. It is used as a metric for engine performance independent
@@ -234,16 +306,16 @@ def calculate_cstar(gamma, temperature, molecular_weight):
         np.power(2 / (gamma + 1), -(gamma + 1) / (2*(gamma - 1)))
         )
 
-def calculate_mach_from_area_ratio_subsonic(gamma, ratio):
+def get_mach_from_subsonic_area_ratio(gamma, ratio):
     '''
     For a given area ratio, calculate the mach in the smaller area given the mach number in the larger area.
     There are two possible solutions for a given ratio (one subsonic, one supersonic). 
     This function always returns the subsonic solution.
     '''
-    return brentq(lambda mach: calculate_area_ratio_from_mach(gamma, mach)-ratio, 0, 1.0)
+    return brentq(lambda mach: get_area_ratio_from_mach_num(gamma, mach)-ratio, 0, 1.0)
     
 
-def calculate_mach_from_area_ratio_supersonic(gamma, ratio):
+def get_mach_from_supersonic_area_ratio(gamma, ratio):
     '''
     For a given area ratio, calculate the mach in the smaller area given the mach number in the larger area.
     There are two possible solutions for a given ratio (one subsonic, one supersonic). 
@@ -252,21 +324,21 @@ def calculate_mach_from_area_ratio_supersonic(gamma, ratio):
 
     #ideally, should be inf, but this would not converge for certain ill-conditioned edge cases.
     upper_mach_limit = 10,000,000.0 
-    return brentq(lambda mach: calculate_area_ratio_from_mach(gamma, mach)-ratio, 1.0, upper_mach_limit)
+    return brentq(lambda mach: get_area_ratio_from_mach_num(gamma, mach)-ratio, 1.0, upper_mach_limit)
 
-def calculate_area_ratio_from_mach(gamma, M):
+def get_area_ratio_from_mach_num(gamma, M):
     '''
     Area ratio (w.r.t throat) for a given mach number and isentropic expansion factor.
     '''
     return 1/M * ((1+ (gamma-1)/2*M**2) /((gamma+1)/2))**((gamma+1)/(2*(gamma-1)))
 
-def calculate_isp(gas, gamma, enthalpy):
+def get_isp(gas, gamma, enthalpy):
     '''
     Calculate specific impulse given an exhaust gas thermodynamic state and combustion chamber enthalpy. 
     '''
     derivs = get_thermo_derivatives(gas)
-    velocity = _calculate_velocity(gas, enthalpy)
-    cstar = calculate_cstar(gamma, gas.T, gas.mean_molecular_weight)
+    velocity = _get_velocity(gas, enthalpy)
+    cstar = get_cstar(gamma, gas.T, gas.mean_molecular_weight)
     CF = velocity/cstar
     g0 = 9.80655 #gravitational acceleration
     return velocity, velocity/g0

goddard/thermo.py
@@ -21,11 +21,13 @@ def get_thermo_derivatives(gas):
     # total unknowns: 2*n_elements + 2
 
     num_var = 2 * gas.n_elements + 2
+    
 
     coeff_matrix = np.zeros((num_var, num_var))
     right_hand_side = np.zeros(num_var)
 
     tot_moles = 1.0 / gas.mean_molecular_weight
+    # print(f"Mean molecular weight: {gas.mean_molecular_weight}")
     moles = gas.X * tot_moles
 
     condensed = False
@@ -67,6 +69,14 @@ def get_thermo_derivatives(gas):
     for i in range(gas.n_elements):
         coeff_matrix[2*gas.n_elements+1, gas.n_elements+1+i] = np.sum(stoich_coeffs[i, :] * moles)
     right_hand_side[2*gas.n_elements+1] = np.sum(moles)
+
+    # print("DEBUG:")
+    # print(f"{coeff_matrix}")
+    # print("Moles")
+    # print(f"{moles}")
+    # print("Enthalpies")
+    # print(f"{gas.standard_enthalpies_RT}")
+    # print("------------------")
     
     derivs = np.linalg.solve(coeff_matrix, right_hand_side)
 
@@ -76,7 +86,7 @@ def get_thermo_derivatives(gas):
     dlogn_dlogP_T = derivs[idx_dlogn_dlogP_T]
 
     # dpi_dlogP_T is not used
-    
+        
     return dpi_dlogT_P, dlogn_dlogT_P, dlogn_dlogP_T
 
 def _get_thermo_properties(gas, dpi_dlogT_P, dlogn_dlogT_P, dlogn_dlogP_T):
@@ -94,6 +104,17 @@ def _get_thermo_properties(gas, dpi_dlogT_P, dlogn_dlogT_P, dlogn_dlogP_T):
         for j, sp in enumerate(gas.species_names):
             stoich_coeffs[i,j] = gas.n_atoms(sp, elem)
     
+    # print("DEBUG PROPERTIES:")
+    # print("Moles")
+    # print(f"{moles}")
+    # print("Enthalpies")
+    # print(f"{gas.standard_enthalpies_RT}")
+    # print("Heat capacities")
+    # print(f"{gas.standard_cp_R}")
+    # print(f"Volume: {gas.v}")
+    # print(f"R: {ct.gas_constant}")
+    # print("------------------")
+
     spec_heat_p = ct.gas_constant * (
         np.sum([dpi_dlogT_P[i] * 
                 np.sum(stoich_coeffs[i,:] * moles * gas.standard_enthalpies_RT) 
@@ -113,9 +134,10 @@ def _get_thermo_properties(gas, dpi_dlogT_P, dlogn_dlogT_P, dlogn_dlogP_T):
 
     gamma = spec_heat_p / spec_heat_v
     gamma_s = -gamma/dlogV_dlogP_T
-    
+    print(f"{gamma_s}, {spec_heat_p}")
     return dlogV_dlogT_P, dlogV_dlogP_T, spec_heat_p, gamma_s
 
 def get_thermo_properties(gas):
     derivs = get_thermo_derivatives(gas)
+    print(derivs)
     return _get_thermo_properties(gas, *derivs)