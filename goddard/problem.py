b82b70ff77df0251cea547a2d01640926bcd81fa
Liem Dam-Quang <liem.dam-quang@mail.mcgill.ca>
2023-11-04 22:39:13 -0700

    Changed RocketProblem to accomodate an iterable of pressure rather than a scalar value

--- goddard/problem.py
+++ goddard/problem.py
@@ -1,18 +1,18 @@
 from abc import ABC, abstractmethod
 from pint import Quantity
 from dataclasses import dataclass
+import numpy as np
 
 import cantera as ct
 
-
 from . import utils
 from .combustion import Combustor
-from .nozzle import Nozzle, ExitConditions
+from .nozzle import Nozzle, ExpansionConditions
 from .result import Result
 
 class Problem:
-    
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, data_files = None, **kwargs) -> None:
+        self.files = data_files
         self.info = kwargs
     
     def report() -> None:
@@ -35,33 +35,34 @@ class NozzleProblem(Problem):
         
         
 class RocketProblem(Problem):
-    def __init__(self, fuel: str, oxidizer: str, pressure: float | Quantity, mixture_ratio, exit_conditions: ExitConditions, combustor, nozzle, **kwargs) -> None:
+    def __init__(self, fuel: str | ct.Solution, oxidizer: str | ct.Solution, pressure: float | Quantity, mixture_ratio, exit_conditions: ExpansionConditions, combustor, nozzle, **kwargs) -> None:
         super().__init__(**kwargs)
-        pressure_SI = utils.normalize_input_units_to_si(pressure)
+        pressure_SI = map(utils.normalize_input_units_to_si, pressure)
+        pressure_array = np.fromiter(pressure_SI)
         #TODO: accept cantera objects directly as well?
-        self.fuel = utils.generate_ct_solution_from_text_input(fuel, pressure_SI) 
-        self.oxidizer = utils.generate_ct_solution_from_text_input(oxidizer, pressure_SI)
+        self.fuel = utils.generate_ct_solution_from_text_input(fuel, pressure_array[0]) 
+        self.oxidizer = utils.generate_ct_solution_from_text_input(oxidizer, pressure_array[0])
         self.mixture_ratio = mixture_ratio(self.fuel, self.oxidizer)
         self.exit_conditions = exit_conditions
         self.combustor = combustor(self.fuel, self.oxidizer, self.mixture_ratio)
         self._nozzle_builder = nozzle
         
     def solve(self) -> Result:
-        combustor_gas = self.combustor.solve()
-        self.nozzle = self._nozzle_builder(combustor_gas, self.exit_conditions)
+        combustor_states = self.combustor.solve()
+        self.nozzle = self._nozzle_builder(self.combustor.reactants, combustor_states, self.exit_conditions)
         output = self.nozzle.get_exit_conditions()
+        return Result(output)
         
 class KineticProblem(Problem):
-    pass
-
-class Solution:
     pass
 
 @dataclass
 class SolverOptions:
     ionized: bool = False
     transport: bool = False
-    use_mole_frac: bool = False
+    mole_basis: bool = False
+    verbose: bool = False
+    debug_output: bool = False
     
-def solve(problem: Problem, options: SolverOptions):
+def solve(problem: Problem, options: SolverOptions = SolverOptions()):
     pass
