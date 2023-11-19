672dae33d4ef5d1d98e94bb93dc347e54c58e6b7
Liem Dam-Quang <liem.dam-quang@mail.mcgill.ca>
2023-11-19 12:08:24 -0800

    Update module files

--- goddard/__init__.py
+++ goddard/__init__.py
@@ -1,2 +1,4 @@
-import problem
-from isp import *
+from goddard.isp import *
+from goddard.nozzle import frozen_nozzle, equilibrium_nozzle, pressure_ratio, subsonic_ratio, supersonic_ratio
+from goddard.combustion import phi_ratio, OF_ratio, equiv_ratio, fuel_pct
+from goddard.problem import CombustionProblem, NozzleProblem, RocketProblem, SolverOptions, solve
