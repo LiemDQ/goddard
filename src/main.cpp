6dbfaf240a520a5b6b105b2ea6ef3e9527601ec4
Liem Dam-Quang <liem.dam-quang@mail.mcgill.ca>
2024-07-05 20:03:14 -0700

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include "cantera/core.h"
-
+#include "cantera/base/SolutionArray.h"
+#include "goddard/equilibrium.hpp"
+#include "goddard/thermo_states.hpp"
 #include <iostream>
 
 int main() {
-
-    std::cout << "I am using Cantera!" << std::endl;
+    auto sln = Cantera::newSolution("h2o2.yaml", "ohmech");
+    double temp = 2400.0; //K
+    double pressure = 50.0*Cantera::OneAtm;
+    auto gas = sln->thermo();
     
+    gas->setState_TPX(temp, pressure, "H2O:1, N2:1, O2:1. AR:0.1"); //completely random composition lol
+    auto slnarr = Cantera::SolutionArray::create(sln, 10);
+
+
+    auto coeffs = Goddard::get_stoichiometric_coeffs(*sln);
+
+    auto manager = Goddard::ThermoArray(sln, std::vector<long>{1,1,1});
+
     return 0;
 }
