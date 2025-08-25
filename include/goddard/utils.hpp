e27bc7888ce787178f67fe0c5ee22ccf254e66df
Liem Dam-Quang <liem.dam-quang@mail.mcgill.ca>
2025-08-24 22:20:15 -0700

    add utils to compilation build

--- include/goddard/utils.hpp
+++ include/goddard/utils.hpp
@@ -3,12 +3,21 @@
 #include <memory>
 #include <cmath>
 #include <algorithm>
-
+#include <string>
+#include <vector>
 namespace Goddard {
+
+std::vector<double> save_gas_state(const Cantera::ThermoPhase& sol);
 
 std::shared_ptr<Cantera::Solution> create_mixture_solution(Cantera::Solution& sol1, Cantera::Solution& sol2);
 
-std::shared_ptr<Cantera::Solution> speciate(const std::string& infile, const std::string& name);
+std::shared_ptr<Cantera::Solution> copy_solution(Cantera::Solution& sln);
+
+/**
+ * @brief Generate a solution object containing all species containing only the provided elements.
+ * 
+ */
+std::shared_ptr<Cantera::Solution> speciate(const std::string& source, const std::vector<std::string>& elements);
 
 constexpr double DEFAULT_RELTOL = 1e-6;
 constexpr double DEFAULT_ABSTOL = 1e-9;
--- src/utils.cpp
+++ src/utils.cpp
@@ -2,21 +2,47 @@
 #include "cantera/base/AnyMap.h"
 #include "cantera/base/global.h"
 #include "cantera/core.h"
+#include <vector>
 
 using Cantera::AnyMap;
 
 namespace Goddard {
 
+std::vector<double> save_thermo_state(const Cantera::ThermoPhase& thermo) {
+    std::vector<double> state(thermo.stateSize());
+    thermo.saveState(state);
+    return state;
+}
+
 std::shared_ptr<Cantera::Solution> create_mixture_solution(Cantera::Solution& sol1, Cantera::Solution& sol2){
     auto thermo1 = sol1.thermo();
     auto thermo2 = sol2.thermo();
 
-    AnyMap params;
-
     AnyMap params = thermo1->parameters();
     AnyMap params2 = thermo2->parameters();
     params.update(params2);
+
+    
+    std::shared_ptr<Cantera::Solution> new_solution = Cantera::newSolution(params);
+    return new_solution;
 }
+
+std::shared_ptr<Cantera::Solution> copy_solution(Cantera::Solution& sln) {
+    std::vector<double> state(sln.thermo()->stateSize());
+    sln.thermo()->saveState(state);
+
+    std::shared_ptr<Cantera::Solution> p_new_sln = Cantera::newSolution(sln.source(), sln.name());
+    p_new_sln->thermo()->restoreState(state);
+    //TODO: is this appropriate?
+    p_new_sln->setTransport(sln.transport());
+    p_new_sln->setKinetics(sln.kinetics());
+
+    return p_new_sln;
+}
+
+// std::shared_ptr<Cantera::Solution> speciate(const std::string& source, const std::vector<std::string>& elements) {
+    
+// }
 
 double check_reltol(double reltol) {
     if (reltol <= 0) {
