5db98740228b45c396fa9ff3c892b21bd9c13c87
Liem Dam-Quang <liem.dam-quang@mail.mcgill.ca>
2026-03-18 13:30:31 -0700

    Implement diluted combustor with accompanying unit tests

--- python/src/bind_combustor.cpp
+++ python/src/bind_combustor.cpp
@@ -9,7 +9,10 @@ namespace nb = nanobind;
 using namespace nb::literals;
 
 void bind_combustor(nb::module_& m) {
-    nb::class_<Goddard::Combustor>(m, "Combustor")
+    nb::class_<Goddard::BaseCombustor>(m, "BaseCombustor")
+        .def("get_combustion_species", &Goddard::BaseCombustor::get_combustion_species);
+
+    nb::class_<Goddard::Combustor, Goddard::BaseCombustor>(m, "Combustor")
         .def(nb::init<std::shared_ptr<Cantera::Solution>,
                        std::vector<double>&,
                        std::vector<double>&>(),
@@ -35,7 +38,41 @@ void bind_combustor(nb::module_& m) {
         .def("generate_mass_fraction_matrix",
              &Goddard::Combustor::generate_mass_fraction_matrix,
              "mixture_ratios"_a)
-        .def("get_combustion_species", &Goddard::Combustor::get_combustion_species)
         .def_rw("fuel_state", &Goddard::Combustor::fuel_state)
         .def_rw("oxidizer_state", &Goddard::Combustor::oxidizer_state);
+
+    nb::class_<Goddard::DilutedCombustor, Goddard::BaseCombustor>(m, "DilutedCombustor")
+        .def(nb::init<std::shared_ptr<Cantera::Solution>,
+                       std::vector<double>&,
+                       std::vector<double>&,
+                       std::vector<double>&>(),
+             "solution"_a, "fuel_state"_a, "oxidizer_state"_a,
+             "flue_state"_a)
+        .def("solve",
+             nb::overload_cast<const Eigen::ArrayXd&,
+                               const Goddard::MixtureRatios&,
+                               double,
+                               const Goddard::CombustorOptions&>(
+                 &Goddard::DilutedCombustor::solve),
+             "pressures"_a, "mixture_ratios"_a, "recirculation_ratio"_a,
+             "options"_a = Goddard::CombustorOptions{})
+        .def("solve_with_temperatures",
+             nb::overload_cast<const Eigen::ArrayXd&,
+                               const Eigen::ArrayXd&,
+                               const Goddard::MixtureRatios&,
+                               double,
+                               const Goddard::CombustorOptions&>(
+                 &Goddard::DilutedCombustor::solve),
+             "temperatures"_a, "pressures"_a, "mixture_ratios"_a,
+             "recirculation_ratio"_a,
+             "options"_a = Goddard::CombustorOptions{})
+        .def("generate_mole_fraction_matrix",
+             &Goddard::DilutedCombustor::generate_mole_fraction_matrix,
+             "mixture_ratios"_a, "recirculation_ratio"_a)
+        .def("generate_mass_fraction_matrix",
+             &Goddard::DilutedCombustor::generate_mass_fraction_matrix,
+             "mixture_ratios"_a, "recirculation_ratio"_a)
+        .def_rw("fuel_state", &Goddard::DilutedCombustor::fuel_state)
+        .def_rw("oxidizer_state", &Goddard::DilutedCombustor::oxidizer_state)
+        .def_rw("flue_state", &Goddard::DilutedCombustor::flue_state);
 }
