0cb23ed3a31443ef7e559878af152408f6718387
Liem Dam-Quang <liem.dam-quang@mail.mcgill.ca>
2026-04-08 09:20:31 -0700

    Implement mach-area relation

--- include/goddard/gas_dynamics.hpp
+++ include/goddard/gas_dynamics.hpp
@@ -55,6 +55,11 @@ double ivac(const Cantera::ThermoPhase& gas, double gamma, double enthalpy);
 double mach(const Cantera::ThermoPhase& gas, double H_stag, double gamma);
 
 /**
+ * For a perfect gas, calculates the area ratio for a given Mach number 
+ * using the Area-Mach number relation. 
+ */
+double mach_area_relation(double mach, double gamma);
+/**
  * @brief Calculate thrust coefficient.
  */
 double C_F(double gamma, double pressure_ratio, double area_ratio);
