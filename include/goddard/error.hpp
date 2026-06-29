5df0de6bb0075559a9983a4967bf4bfc1daf8ba7
Liem Dam-Quang <dq@liem.ca>
2026-06-29 15:26:42 -0400

    Remove redundant virtual keywords

--- include/goddard/error.hpp
+++ include/goddard/error.hpp
@@ -10,7 +10,7 @@ class NotImplementedError: public std::logic_error {
     using std::logic_error::logic_error;
 
     public:
-    virtual const char * what() const noexcept override;
+    const char * what() const noexcept override;
 
 };
 
@@ -19,7 +19,7 @@ class ConvergenceError: public std::runtime_error {
 
 public:
     ConvergenceError(const std::string& what, int num_iters = -1, double tol = -1.0, double residual = -1.0);
-    virtual const char * what() const noexcept override;
+    const char * what() const noexcept override;
 
 private:
     int m_num_iters;
